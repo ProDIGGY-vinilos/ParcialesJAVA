4ccde985c678b1d0730d234b052f8afa718253a1
ProDIGGY-vinilos <cristian_gerster@hotmail.com>
2022-08-17 14:17:06 -0300

    parcial2021 terminated

--- parcial2021/src/parcial2021/BillItem.java
+++ parcial2021/src/parcial2021/BillItem.java
@@ -1,7 +1,34 @@
 package parcial2021;
 
-public class BillItem {
-	public double getUnitPrice() {return 1;}
-	public int getQuantity() {return 1;}
+import java.util.LinkedList;
+
+public class BillItem  {
+	private double unitPrice;
+	private int quantity;
+	
+	public BillItem(double unitPrice, int quantity) {
+		super();
+		this.unitPrice = unitPrice;
+		this.quantity = quantity;
+	}
+	public void setUnirPrice(double unirPrice) {
+		this.unitPrice = unirPrice;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getUnitPrice() {return unitPrice;}
+	public int getQuantity() {return quantity;}
+	
+	public static double getTotal(LinkedList<BillItem> items, Boolean includeTax) {
+		double tot =0;
+		for (BillItem billItem : items) {
+			tot = tot + (billItem.getUnitPrice() * billItem.getQuantity());
+		}
+		if (includeTax) {
+			tot= tot*1.21;
+		}
+		return tot;
+	}
 
 }
--- parcial2021/src/parcial2021/Principal.java
+++ parcial2021/src/parcial2021/Principal.java
@@ -2,24 +2,30 @@ package parcial2021;
 
 import java.util.LinkedList;
 import java.util.Scanner;
+import parcial2021.BillItem; 
 
 public class Principal {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		LinkedList<BillItem> it = new LinkedList<>();
+		BillItem b = new BillItem(0, 0);
+		BillItem bill = new BillItem(1, 3);
+		it.add(bill);
+		BillItem bill1 = new BillItem(2, 1);
+		it.add(bill1);
 		
+		BillItem bill2 = new BillItem(1, 5);
+		it.add(bill2);
 		
+		BillItem bill3 = new BillItem(2, 2);
+		it.add(bill3);
+		
+		System.out.println(BillItem.getTotal(it, false));	
+	
 
 	}
-	public double getTotal(LinkedList<BillItem> items, Boolean includeTax) {
-		double tot =0;
-		for (BillItem billItem : items) {
-			tot = tot + (billItem.getUnitPrice() * billItem.getQuantity());
-		}
-		if (includeTax) {
-			tot= tot*1.21;
-		}
-		return tot;
-	}
-
+	
+	
 }
