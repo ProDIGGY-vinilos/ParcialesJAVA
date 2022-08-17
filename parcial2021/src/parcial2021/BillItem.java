47504e9739f27f749f57df223c5800431eda8a10
ProDIGGY-vinilos <cristian_gerster@hotmail.com>
2022-08-17 13:50:12 -0300

    parcial2021 completo

---
+++ parcial2021/src/parcial2021/BillItem.java
@@ -0,0 +1,7 @@
+package parcial2021;
+
+public class BillItem {
+	public double getUnitPrice() {return 1;}
+	public int getQuantity() {return 1;}
+
+}
--- parcial2021/src/parcial2021/Principal.java
+++ parcial2021/src/parcial2021/Principal.java
@@ -1,5 +1,6 @@
 package parcial2021;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Principal {
@@ -9,6 +10,16 @@ public class Principal {
 		
 		
 
+	}
+	public double getTotal(LinkedList<BillItem> items, Boolean includeTax) {
+		double tot =0;
+		for (BillItem billItem : items) {
+			tot = tot + (billItem.getUnitPrice() * billItem.getQuantity());
+		}
+		if (includeTax) {
+			tot= tot*1.21;
+		}
+		return tot;
 	}
 
 }
