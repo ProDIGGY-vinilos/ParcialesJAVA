a5c7643bbba9a9faa2e119316a17229731f22308
ProDIGGY-vinilos <cristian_gerster@hotmail.com>
2022-08-17 10:44:05 -0300

    asd

---
+++ parcial2021/src/entities/Caterig.java
@@ -0,0 +1,5 @@
+package entities;
+
+public class Caterig extends Event {
+
+}
---
+++ parcial2021/src/parcial2021/Menu.java
@@ -0,0 +1,10 @@
+package parcial2021;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
